c01a038c07e5656664a5538ce5e3ec75ba75c9e3
entl210 <edwardlamhitech@gmail.com>
2025-02-23 13:50:30 -0800

    Working on the introductory screen

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { motion } from 'motion/react';
 import styled from 'styled-components'
 import Sidebar from './sidebar-components/Sidebar'
 
@@ -23,18 +24,78 @@ function App() {
   flex-direction: row;
   `
 
-  const Main = styled.div`
-  width: calc(100vw - ${(props) => props.sidebarWidth || '215px'});
+  const Main = styled(motion.div)`
+  //width: calc(100vw - ${(props) => props.sidebarWidth || '215px'});
   background-color: rgba(44,44,43,1);
   border-radius: 10px;
   margin: 7.5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   `
+
+  const WelcomePhraseWrapper = styled(motion.p)`
+    background: linear-gradient(45deg, #ff6347, #4CAF50, #1E90FF, #ff6347);
+    background-size: 400% 400%;
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+    font-size: 50px;
+    font-weight: 700;
+    text-transform: uppercase;
+    
+  `
+
+  const WelcomePhraseAnimation = {
+    hidden: {opacity: 1},
+    visible: {
+      opacity: 1,
+      backgroundPosition: '100% 0%',
+      transition: {
+        delay: 0.5,
+        ease: 'linear',
+        duration: 5,
+        staggerChildren: 0.08,
+        repeat: Infinity,  // Repeat the animation infinitely
+        repeatType: 'loop',
+      }
+    }
+  }
+
+  const WelcomePhraseTextAnimation = {
+    hidden: {opacity: 0},
+    visible: {
+      opacity: 1,
+    }
+  }
+
+  const welcomeText = "Welcome to MicroPycon"
   
 
   return (
     <RootContainer>
       <Sidebar sidebarWidth={`${sidebarWidth}px`} selectedDirectory={selectedFolderPath}/>
-      <Main sidebarWidth={`${sidebarWidth}px`}>
+      <Main 
+        initial = {{
+          width: '100vw',
+          margin: '50px 7.5px 7.5px 7.5px'
+        }}
+      >
+        <WelcomePhraseWrapper
+          variants={WelcomePhraseAnimation}
+          initial="hidden"
+          animate="visible"
+        >
+          {welcomeText.split("").map((char, index) => {
+            return(
+              <motion.span key={char + "-" + index} variants={WelcomePhraseTextAnimation}>
+                {char}
+              </motion.span>
+            )
+          })}
+        </WelcomePhraseWrapper>
+        
       </Main>
     </RootContainer>
   )
--- src/ui/sidebar-components/Sidebar.jsx
+++ src/ui/sidebar-components/Sidebar.jsx
@@ -1,26 +1,29 @@
 import styled from "styled-components";
+import { motion } from "motion/react";
 import FileTab from "./FileTab";
 
 function Sidebar({sidebarWidth, selectedDirectory}) {
-    const Sidebar = styled.div`
-    width: ${sidebarWidth || '215px'};
+    const Sidebar = styled(motion.div)`
+    // width: ${sidebarWidth || '215px'};
     position: relative;
     margin: 50px 0px 7.5px 0;
     padding:5px;
     color:black;
     display: flex;
     justify-content: flex-start;
-    padding: 0px 0px 0px 5px;
     overflow-x: scroll;
     scroll-behavior: smooth;
     scroll-snap-type: x mandatory;
     font-weight: 700;
     `
 
-    console.log(selectedDirectory)
-
     return(
-        <Sidebar>
+        <Sidebar
+        initial={{
+            width: '0px',
+            padding: '0px'
+        }}
+        >
             <FileTab sidebarWidth={sidebarWidth} selectedDirectory={selectedDirectory}/>
         </Sidebar>
     )
