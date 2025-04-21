36de463497f50ba0bceac42e5a83ffa109fb8cb8
entl210 <edwardlamhitech@gmail.com>
2025-04-21 12:57:12 -0700

    Refactor Welcoming Screen, turning it into a whole new component

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -3,9 +3,7 @@ import { motion, AnimatePresence } from 'motion/react';
 import styled from 'styled-components'
 import Sidebar from './sidebar-components/Sidebar'
 import { easeIn, easeOut } from 'motion';
-import fileIcon from './assets/file-icon.png'
-import folderIcon from './assets/folder-icon.png'
-import { object } from 'motion/react-client';
+import WelcomingScreen from './main-component/WelcomingScreen';
 
 function App() {
   const [sidebarWidth, setSidebarWidth] = useState(250);
@@ -75,84 +73,6 @@ function App() {
   justify-content: center;
   `
 
-  const WelcomingScreen = styled(motion.div)`
-    flex-grow: 1;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    user-select: none;
-  `
-
-  const WelcomePhraseWrapper = styled(motion.p)`
-    background: linear-gradient(45deg, #FF75C3, #FFA647, #FFE83F, #9FFF5B, #7EEBCE, #70E2FF);
-    background-size: 400% 400%;
-    color: transparent;
-    -webkit-background-clip: text;
-    background-clip: text;
-    font-size: 40px;
-    font-weight: 700;
-    text-transform: uppercase;
-    text-align: center;
-  `
-
-  const FileDialogLauncherWrapper = styled(motion.div)`
-    position: absolute;
-    bottom: 10%;
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
-    align-items: center;
-  `
-
-  const FileDialogLauncherBtn = styled(motion.button)`
-    background: none;
-    color: inherit;
-    border: none;
-    padding: 0;
-    font: inherit;
-    outline: inherit;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #464950;
-    padding: 10px;
-    border-radius: 50%;
-    
-
-    img {
-      width: 25px;
-      cursor: pointer;
-    }
-  `
-
-  const WelcomePhraseAnimation = {
-    hidden: { opacity: 1 },
-    visible: {
-      opacity: 1,
-      backgroundPosition: '100% 0%',
-      transition: {
-        delay: 0.5,
-        ease: 'easeInOut',
-        duration: 4,
-        staggerChildren: 0.05,
-        repeat: Infinity,  // Repeat the animation infinitely
-        repeatType: 'mirror',
-      }
-    }
-  }
-
-  const WelcomePhraseTextAnimation = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-    }
-  }
-
-  const welcomeText = "Welcome to MicroPycon"
-
-
   return (
     <RootContainer>
       <Sidebar sidebarWidth={`${sidebarWidth}px`} selectedDirectory={selectedFolder} openedFilesArr={openedFilesArr}/>
@@ -174,71 +94,7 @@ function App() {
 
 
         {(selectedFilePath.length == 0 && selectedFolderPath.length == 0) && (
-          <WelcomingScreen
-            initial={{
-              opacity: 0,
-            }}
-            animate={{
-              opacity: 1,
-            }}
-            exit={{
-              opacity: 0
-            }}
-            transition={{
-              ease: easeOut,
-              duration: 1
-            }}
-          >
-            <WelcomePhraseWrapper
-              variants={WelcomePhraseAnimation}
-              initial="hidden"
-              animate="visible"
-            >
-              {welcomeText.split("").map((char, index) => {
-                return (
-                  <motion.span key={char + "-" + index} variants={WelcomePhraseTextAnimation}>
-                    {char}
-                  </motion.span>
-                )
-              })}
-            </WelcomePhraseWrapper>
-            <FileDialogLauncherWrapper>
-              <FileDialogLauncherBtn
-                whileHover={{
-                  backgroundColor: "rgba(46,150,255, 1)"
-                }}
-                onClick={async () => {
-                  try {
-                    const filePath = await window.electron.openFileDialog();
-                    if (filePath.length > 0) {
-                      setSelectedFilePath(filePath)
-                    }
-                  } catch (err) {
-                    console.log("Failed to open dialog", err)
-                  }
-                }}
-              >
-                <img src={fileIcon} alt="" />
-              </FileDialogLauncherBtn>
-              <FileDialogLauncherBtn
-                whileHover={{
-                  backgroundColor: "rgba(46,150,255, 1)",
-                }}
-                onClick={async () => {
-                  try {
-                    const folderPath = await window.electron.openDirectoryDialog();
-                    if (folderPath.length > 0) {
-                      setSelectedFolderPath(folderPath[0])
-                    }
-                  } catch (err) {
-                    console.log("Failed to open dialog", err)
-                  }
-                }}
-              >
-                <img src={folderIcon} alt="" />
-              </FileDialogLauncherBtn>
-            </FileDialogLauncherWrapper>
-          </WelcomingScreen>
+          <WelcomingScreen setSelectedFilePath={setSelectedFilePath} setSelectedFolderPath={setSelectedFolderPath}/>
         )}
 
       </Main>
